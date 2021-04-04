0d461fa323b1f558cd4d9248f845aad543e890a4
yukina1230 <lost.laugh1230@gmail.com>
2021-04-04 23:56:11 +0000

    [Finish]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,10 +3,11 @@ class SearchesController < ApplicationController
 
   def search
     @range = params[:range]
+    @word = params[:word]
 
-    if @range == "User"
+    if @range == 'User'
       @users = User.looks(params[:search], params[:word])
-    elsif @range == "Book"
+    else
       @books = Book.looks(params[:search], params[:word])
     end
   end
